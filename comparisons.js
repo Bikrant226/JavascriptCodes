37624ddf2a2420d5061b7dfc0e4f1d8a6bc953c6
Bikrant226 <bikrantthakur6@gmail.com>
2023-06-09 22:02:59 +0530

    added comparison codes

---
+++ comparisons.js
@@ -0,0 +1,15 @@
+//1.Comparisons
+
+// == converts operands to one specific type whereas === compares both type and value
+console.log(0==false)// because operands of different types are converted to number
+console.log(0===false)// so we use strict equality operator
+
+console.log(''==false)
+console.log(''===false)
+
+// == converts operands to one specific type whereas === compares both type and value
+console.log('2'==2); //true
+console.log('2'===2); //false
+
+
+
